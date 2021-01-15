f25c929d7b5408dce3ed27dc7318472ee6dc4f96
ERICH SAUER <erich.sauer@gmail.com>
2021-01-15 14:02:33 -0800

    function one

--- src/math-functions.js
+++ src/math-functions.js
@@ -12,7 +12,8 @@ example and uses the values that were input into the function:
 */
 
 export function sum(a, b) {
-
+    let someArray;
+    return someArray = [a + b, `The sum of ${a} and ${b} is ${a + b}.`];
 }
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
